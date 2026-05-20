64fc12bb5ecd27d875daac26d51bd5a0ba6a603d
Samuel David Pinzón Fernandez <pinzonsam139@gmail.com>
2026-05-19 20:13:43 -0500

    Create proyect.py

---
+++ proyect.py
@@ -0,0 +1,25 @@
+print("Este programa realiza un analisis completo de todos los numeros a ingresar ")
+lista=[]
+while True :
+    try :
+        number = int(input("Ingresa en numero:"))
+        lista.append(number)
+    except ValueError:
+        print("Error al ingresar se esperaba un dato tipo entero.")
+    salir = input("ingresa s para salir o cualquier letra para continuar:")
+    if salir.lower() == "s":
+        break
+cantidad_numbers = len(lista)
+if len(lista)>0:
+    total = sum(lista)
+    prom = total/len(lista)
+    maximo = max(lista)
+    minimo = min(lista)
+    print(f"La cantidad de datos ingresados es: {cantidad_numbers}.")
+    print(f"La suma total de los datos ingresados es de: {total}.")
+    print(f"El promedio fue de: {prom:.2f}.")
+    print(f"El numero mayor ingresado fue: {maximo}.")
+    print(f"El numero minimo ingresado fue: {minimo}.")
+    with open("Numeros.txt","a") as Numeros:
+        Numeros.write(f"Nuevo Ingreso....\n")
+        Numeros.write(f"La cantidad de datos ingresados es: {cantidad_numbers}.\n La suma total de los datos ingresados es de: {total}.\n El promedio fue de: {prom:.2f}.\n El numero mayor ingresado fue: {maximo}.\nEl numero minimo ingresado fue: {minimo}")
